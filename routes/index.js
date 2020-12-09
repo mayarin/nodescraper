04f7ef953c1188e769fcda9d01d385943c904c25
Maya <mayarin@users.noreply.github.com>
2020-12-09 19:59:44 +0900

    20201209

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
 var express = require('express');
-const url = require('url')
+const https = require('https');
+const url = 'https://maya-pg.net';
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  const proxyUrl = url.parse('https://maya-pg.net')
-  proxyUrl.rejectUnauthorized = false // 証明書によるエラーを無視
+  https.get(url, res => {
+    let html = '';
+    res.on('data', line => html += line);
+    res.on('end', () => {
+      console.log(html);
+    });
+  });
 
-  console.log(proxyUrl);
-
-  // res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
