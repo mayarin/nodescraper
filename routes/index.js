aaa387316544a6e654590e5c1d8618d72ad303a0
Maya <mayarin@users.noreply.github.com>
2020-12-09 19:54:57 +0900

    20201209

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  const proxyUrl = url.parse('https://maya-pg.net')
+  proxyUrl.rejectUnauthorized = false // 証明書によるエラーを無視
+
+  AWS.config.update({
+    httpOptions: {
+      agent: HttpsProxyAgent(proxyUrl)
+    }
+  })
 
 
-
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
